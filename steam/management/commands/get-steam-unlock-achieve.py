9b0952854da30b34804e5980bb077b6f07125431
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-13 14:39:02 +0900

    ゲーム実績情報取得追加

--- steam/management/commands/get-steam-unlock-achieve.py
+++ steam/management/commands/get-steam-unlock-achieve.py
@@ -11,7 +11,9 @@ from steam.models import App, Achievement
 STEAM_API_KEY = os.environ.get("STEAM_API_KEY")
 STEAM_ID = os.environ.get("STEAM_ID")
 RECENTLY_URL = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key={}&steamid={}&format=json"
-PLAYER_ACHIEVEMENTS_URL = "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={}&key={}&steamid={}&l=japanese"
+SCHEMA_FOR_GAME_URL = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key={}&appid={}&l=japanese"
+PLAYER_ACHIEVEMENTS_URL = "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/" \
+                          "?appid={}&key={}&steamid={}&l=japanese"
 
 
 class Command(BaseCommand):
@@ -35,6 +37,7 @@ class Command(BaseCommand):
                 app = App(app_id=game["appid"], name=game["name"])
                 app.save()
 
+            # プレイヤー実績取得
             res = requests.get(PLAYER_ACHIEVEMENTS_URL.format(game["appid"], STEAM_API_KEY, STEAM_ID))
             if res.status_code == 400:
                 print(json.loads(res.content)["playerstats"]["error"])
@@ -50,9 +53,18 @@ class Command(BaseCommand):
                 print("no achievements")
                 continue
 
+            # ゲーム実績情報取得
+            res = requests.get(SCHEMA_FOR_GAME_URL.format(STEAM_API_KEY, game["appid"]))
+            print(res.content.decode(encoding='utf-8'))
+            schemas = json.loads(res.content)["game"]["availableGameStats"]["achievements"]
+
             achievements = player_stats["achievements"]
             print(achievements)
             for achievement in achievements:
+                try:
+                    schema = [x for x in schemas if x["name"] == achievement["apiname"]][0]
+                except IndexError:
+                    schema = {}
                 try:
                     achieve = Achievement.objects.get(app=app, api_name=achievement["apiname"])
                 except Achievement.DoesNotExist:
@@ -61,86 +73,6 @@ class Command(BaseCommand):
                 achieve.achieved = bool(achievement["achieved"])
                 achieve.unlock_time = make_aware(datetime.fromtimestamp(achievement["unlocktime"]))
                 achieve.description = achievement["description"]
+                achieve.icon = schema.get("icon")
+                achieve.hidden = bool(schema.get("hidden"))
                 achieve.save()
-
-# import pickle
-# import time
-# import math
-# from datetime import datetime, timedelta
-
-# PLAYER_ACHIEVEMENTS_URL = "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid={0}&key={1}&steamid={2}&l=japanese"
-# SCHEMA_FOR_GAME = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key={0}&appid={1}&l=japanese"
-# DUMP_FILE = "/home/akaneskmz/www/get-steam-unlock-achieve.dump"
-# IMG_DIR = "/home/akaneskmz/www/"
-# CONVERT = "/usr/local/bin/convert"
-# MONTAGE = "/usr/local/bin/montage"
-#
-# # ローカル実行用
-# if os.name == "nt":
-# 	DUMP_FILE = "D:\\dt\\www\\get-steam-unlock-achieve.dump"
-# 	IMG_DIR = "D:\\dt\\www\\"
-# 	CONVERT = "D:\\fs\\ImageMagick-7.0.7-9-portable-Q16-x64\\convert"
-# 	MONTAGE = "D:\\fs\\ImageMagick-7.0.7-9-portable-Q16-x64\\montage"
-#
-# # 現在時刻
-# now = datetime.now()
-#
-# # データ初期化
-# dmp = {}
-# if os.path.exists(DUMP_FILE):
-# 	f = open(DUMP_FILE, 'r')
-# 	dmp = pickle.load(f)
-# 	f.close()
-# else:
-# 	dmp['lasttime'] = now + timedelta(days=-1)
-# 	dmp['forrss'] = []
-# lasttime = int(time.mktime(dmp['lasttime'].timetuple()))
-# dmp['lasttime'] = now
-#
-
-#
-# 	paAchieve = paData['playerstats']['achievements']
-# 	newAchieve = [x for x in paAchieve if x['achieved']==1 and x['unlocktime'] > lasttime]
-#
-# 	# 実績未取得はスキップ
-# 	if not newAchieve:
-# 		continue
-#
-# 	forRssData = {}
-# 	forRssData['time'] = now
-# 	forRssData['appid'] = appid
-# 	forRssData['game'] = gameName
-#
-# 	guid = str(int(time.mktime(now.timetuple()))) + "_" + str(appid)
-# 	forRssData['guid'] = guid
-#
-# 	# ゲームの実績詳細情報を取得
-# 	schemaJson = urllib2.urlopen(SCHEMA_FOR_GAME.format(API_KEY, appid)).read()
-#
-# 	schemaData = json.loads(schemaJson)
-#
-# 	achiDtl = schemaData['game']['availableGameStats']['achievements']
-#
-# 	forRssData['achieves'] = []
-# 	cnt = 0
-#
-# 	for i in range(len(newAchieve)):
-# 		ac = newAchieve[i]
-# 		ad = [x for x in achiDtl if x['name'] == ac['apiname']][0]
-# 		forRssData['achieves'].append({'name': ac['name'], 'desc': ad['description'] if 'description' in ad else '-'})
-# 		urllib.urlretrieve(ad['icon'], IMG_DIR + "ac" + guid + "_" + str(i) + ".jpg")
-# 		cnt += 1
-# 		# print guid + "_" + str(i) + ".jpg"
-#
-# 	n = int(math.ceil((cnt+1)/12.0))
-# 	w = int(math.ceil(math.sqrt(n))) * 4
-# 	h = int(math.ceil(cnt/float(w)))
-# 	os.system(MONTAGE + " " + IMG_DIR + "ac" + guid + "_* -tile " + str(w) + "x" + str(h) + " -geometry +0+0 " + IMG_DIR + "ac" + guid + ".jpg")
-#
-# 	dmp['forrss'].append(forRssData)
-#
-# #print dmp['forrss']
-#
-# f = open(DUMP_FILE, 'w')
-# pickle.dump(dmp, f)
-# f.close()
