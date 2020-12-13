c949285a5bb3dab3d7af866433aaa7ec107056bd
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-14 02:46:22 +0900

    不要アプリ削除

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 admin.autodiscover()
-
-import hello.views
 
 # To add a new path, first import the app:
 # import blog
@@ -15,8 +14,7 @@ import hello.views
 # Learn more here: https://docs.djangoproject.com/en/2.1/topics/http/urls/
 
 urlpatterns = [
-    path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
-    path("steam/", include('steam.urls')),
+    path("", RedirectView.as_view(url="steam/")),
+    path("steam/", include('steam.urls'), name="steam"),
     path("admin/", admin.site.urls),
 ]
