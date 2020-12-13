acce55ea41fe97ebb48d31dbe0eff97b1e865115
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-13 14:49:03 +0900

    実績リスト表示

--- steam/views.py
+++ steam/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from steam.models import Achievement
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "steam/index.html"
+    queryset = Achievement.objects.filter(achieved=True).order_by('-unlock_time')
