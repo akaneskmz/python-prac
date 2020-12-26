dbecb6b15408f15a8c2d8d637279a14bd6212ca2
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-26 22:28:08 +0900

    ゲームリスト取得：ゲーム名も取得

--- steam/management/commands/get-steam-owned-games.py
+++ steam/management/commands/get-steam-owned-games.py
@@ -3,7 +3,7 @@ import os
 import requests
 from django.core.management import BaseCommand, CommandError
 
-from steam.models import OwnedGames
+from steam.models import OwnedGames, GameList
 
 STEAM_API_KEY = os.environ.get("STEAM_API_KEY")
 STEAM_ID = os.environ.get("STEAM_ID")
@@ -12,6 +12,7 @@ GET_OWNED_GAMES_URL = "http://api.steampowered.com/IPlayerService/GetOwnedGames/
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        # 所持ゲーム取得
         print(GET_OWNED_GAMES_URL)
         res = requests.get(GET_OWNED_GAMES_URL.format(STEAM_API_KEY, STEAM_ID))
 
@@ -19,9 +20,56 @@ class Command(BaseCommand):
             print("get owned games error: {}".format(res.status_code))
             raise CommandError(res.content.decode(encoding='utf-8'))
 
+        # ゲームリスト
+        game_list = GameList.objects.first()
+        if game_list:
+            apps = game_list.data["apps"]
+        else:
+            apps = []
+        # print(apps)
+
+        def get_app_name(app_id):
+            """appidからゲーム名取得"""
+            for app in apps:
+                if app["appid"] == app_id:
+                    return app["name"]
+            return None
+
+        # 保存済みリスト取得
         own = OwnedGames.objects.first()
         if own:
-            own.data = res.json()["response"]
+            owned_games_old = own.data["games"]
         else:
-            own = OwnedGames(data=res.json()["response"])
+            owned_games_old = []
+        # print(owned_games_old)
+
+        def get_owned_game_old(app_id):
+            """appidから保存済みのゲームデータ取得"""
+            for game in owned_games_old:
+                if game["appid"] == app_id:
+                    return game
+            return {}
+
+        owned_games = res.json()["response"]
+        print(owned_games)
+
+        # 保存用データ作成
+        new_owned_games = {"game_count": owned_games["game_count"], "games": []}
+
+        for owned_game in owned_games["games"]:
+            game_data = get_owned_game_old(owned_game["appid"])
+            game_data.update(owned_game)
+
+            # ゲーム名追加
+            if not game_data.get("name"):
+                game_data["name"] = get_app_name(game_data["appid"])
+
+            new_owned_games["games"].append(game_data)
+
+        # データ保存
+        if own:
+            own.data = new_owned_games
+        else:
+            own = OwnedGames(data=new_owned_games)
+
         own.save()
