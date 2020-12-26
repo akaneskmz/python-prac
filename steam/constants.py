1b26b7ca1ebfc6c2b7cab39f1f03e6bab9ec9911
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-27 00:48:56 +0900

    取得不可ゲーム定義

---
+++ steam/constants.py
@@ -0,0 +1,6 @@
+INVALID_APPS = {312710: {'name': 'Broadsword : Age of Chivalry'}, 335080: {'name': "Biology Battle"},
+                428430: {'name': "Endorlight"}, 488680: {'name': "Tower Defense Ultimate"},
+                492660: {'name': "Featherpunk Prime"}, 526790: {'name': "Monsti"},
+                545230: {'name': "Professor Nasty Time: The Stupidly Unfair Test Simulator 2016"},
+                548930: {'name': "Mankind Defender"}, 562650: {'name': "Cube Master"},
+                570550: {'name': "Fearful Symmetry"}}
