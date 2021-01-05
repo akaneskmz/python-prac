5a428eeea0d36201c9244f8dffb19c23c0ca0d25
akaneskmz <vermilion_sounds@yahoo.co.jp>
2021-01-06 03:10:49 +0900

    金額表記変換

---
+++ steam/templatetags/steam_tags.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def price(val: int, currency: str):
+    if currency == "JPY":
+        return "\\" + "{:,}".format(int(val / 100))
+    elif currency == "USD":
+        return "$" + "{:,}".format(val / 100)
+    else:
+        raise ValueError("unknown currency")
