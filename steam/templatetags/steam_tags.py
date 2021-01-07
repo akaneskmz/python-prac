3b77520e77055b4b07a17b038bb532dbe5a72040
akaneskmz <vermilion_sounds@yahoo.co.jp>
2021-01-07 09:18:41 +0900

    ユーロに対応

--- steam/templatetags/steam_tags.py
+++ steam/templatetags/steam_tags.py
@@ -6,8 +6,10 @@ register = template.Library()
 @register.filter
 def price(val: int, currency: str):
     if currency == "JPY":
-        return "\\" + "{:,}".format(int(val / 100))
+        return "¥ {:,}".format(int(val / 100))
     elif currency == "USD":
-        return "$" + "{:,}".format(val / 100)
+        return "${:,}".format(val / 100)
+    elif currency == "EUR":
+        return "{:,}€".format(val / 100)
     else:
         raise ValueError("unknown currency")
