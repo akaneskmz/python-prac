7130bc578bf6acc9e2f7811ac9bcf25292bce254
akaneskmz <vermilion_sounds@yahoo.co.jp>
2021-02-10 17:27:01 +0900

    スタンプIDを返却

--- line_bot/views.py
+++ line_bot/views.py
@@ -1,6 +1,6 @@
 import os
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 
 # Create your views here.
 from django.utils.decorators import method_decorator
@@ -9,6 +9,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 # 環境変数取得
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
@@ -21,9 +23,23 @@ class IndexView(View):
     def post(self, request, *args, **kwargs):
         signature = request.headers['X-Line-Signature']
         print(signature)
-        print(request.body.decode())
+        body = request.body.decode()
+
+        # handle webhook body
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseBadRequest()
+
         return HttpResponse("OK")
 
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(IndexView, self).dispatch(*args, **kwargs)
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.stickerId))
