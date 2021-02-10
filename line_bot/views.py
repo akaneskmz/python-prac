a8cfdb228bba5eba4fdf08c6451002489a08b10a
akaneskmz <vermilion_sounds@yahoo.co.jp>
2021-02-10 10:31:49 +0900

    CSRF回避

--- line_bot/views.py
+++ line_bot/views.py
@@ -2,10 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils.decorators import method_decorator
 from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
 
 class IndexView(View):
     def post(self, request, *args, **kwargs):
         print(request.POST)
         return HttpResponse("OK")
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(IndexView, self).dispatch(*args, **kwargs)
