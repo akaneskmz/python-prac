73de7c022734608ab24d09b3ec6f140b3f615500
akaneskmz <vermilion_sounds@yahoo.co.jp>
2021-02-10 17:18:28 +0900

    署名取得

--- line_bot/views.py
+++ line_bot/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.http import HttpResponse
 
 # Create your views here.
@@ -5,9 +7,20 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
+# 環境変数取得
+from linebot import LineBotApi, WebhookHandler
+
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
 
 class IndexView(View):
     def post(self, request, *args, **kwargs):
+        signature = request.headers['X-Line-Signature']
+        print(signature)
         print(request.body.decode())
         return HttpResponse("OK")
 
