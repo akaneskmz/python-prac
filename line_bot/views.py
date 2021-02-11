fd50d441ba55ebc293d48fb97e86ffd4c9ea4c97
akaneskmz <vermilion_sounds@yahoo.co.jp>
2021-02-11 13:22:27 +0900

    他のバッチへの影響修正

--- line_bot/views.py
+++ line_bot/views.py
@@ -12,8 +12,8 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextSendMessage, StickerMessage
 
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ.get("YOUR_CHANNEL_ACCESS_TOKEN", "")
+YOUR_CHANNEL_SECRET = os.environ.get("YOUR_CHANNEL_SECRET", "")
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
